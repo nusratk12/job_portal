fbdc2c0dee264c7c0651cf983cb3f7245dae266c
Your Name <you@example.com>
2021-09-21 11:50:31 +0600

    initial commit

---
+++ ONLINE_JOB_PORTAL(MAIN)/Online Job-Portal_main/Online Job-Portal_main/Confirmation/connect.php
@@ -0,0 +1,6 @@
+<?php
+if (isset($_POST['Register'])){
+	echo "<script>alert('Confirmed!')</script>";
+	echo "<script>window.open('http://localhost/online%20Job-portal_main/Homepage/homepage.php','_self')</script>";
+}
+?>
